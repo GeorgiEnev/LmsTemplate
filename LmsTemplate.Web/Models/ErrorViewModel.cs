4aa1ec2ec1c1ae4f66f6a23d1411440e55729821
GeorgiEnev <gogoenev@gmail.com>
2025-11-04 01:44:45 +0200

    Initial commit

---
+++ LmsTemplate.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LmsTemplate.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
