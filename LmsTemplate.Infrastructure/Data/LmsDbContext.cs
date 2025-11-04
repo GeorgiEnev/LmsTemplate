94e90d6405ad581237e44c6f4f4f4593d6c99a58
GeorgiEnev <gogoenev@gmail.com>
2025-11-04 02:16:05 +0200

    configure DbContext and Identity

---
+++ LmsTemplate.Infrastructure/Data/LmsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using LmsTemplate.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LmsTemplate.Infrastructure.Data
+{
+    public class LmsDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public LmsDbContext(DbContextOptions<LmsDbContext> options)
+            : base(options)
+        {
+        }
+
+        // We'll add DbSet<Course>, DbSet<AcademicRole>, etc. here later.
+    }
+}
