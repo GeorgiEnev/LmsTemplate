5c1854f54effde9cb2c9a1891c2860b01e2cf9da
GeorgiEnev <gogoenev@gmail.com>
2025-11-15 23:58:41 +0200

    add Course creation (GET/POST) with form and validation handling

--- LmsTemplate.Web/Controllers/CoursesController.cs
+++ LmsTemplate.Web/Controllers/CoursesController.cs
@@ -1,4 +1,5 @@
-﻿using LmsTemplate.Application.Interfaces;
+﻿using LmsTemplate.Application.Dtos.Courses;
+using LmsTemplate.Application.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,5 +21,30 @@ namespace LmsTemplate.Web.Controllers
             return View(courses);
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateCourseRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(request);
+            }
+
+            try
+            {
+                await _courseService.CreateCourseAsync(request);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return View(request);
+            }
+        }
     }
 }
